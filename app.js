25c276e5fa5226b0239d1ea08fea1983a4de81c6
Nolan Thompson <nolanpanther@yahoo.com>
2016-03-28 19:40:02 -0700

    Adding Files
    New Portfolio Node app

---
+++ app.js
@@ -0,0 +1,21 @@
+/*
+ * Module dependencies
+ */
+var express = require("express"),
+    lessMiddleware = require("less-middleware"),
+    nodemailer = require('nodemailer');
+
+var app = express();
+app.use(lessMiddleware(__dirname + "/less",{
+	dest: __dirname + "/public",
+    debug: true,
+	force: true
+}));
+app.set("views", __dirname + "/views");
+app.set("view engine", "jade");
+app.use(express.logger("dev"));
+app.use(express.static(__dirname + "/public"));
+app.use(express.bodyParser());
+require("./routes")(app, express, nodemailer);
+
+app.listen(3000);
