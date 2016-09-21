5af1884619f3e4961fd5154afa8430ac25cc604d
r3dscar3 <nolanpanther@yahoo.com>
2016-09-20 22:11:05 -0700

    New site changes to side menu

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
-  $('.full-brand').on('click', function() {
+  var sideMenu = false;
+  $('.full-brand, .side-menu-contain .close').on('click', function() {
     $('.header').toggle('fade', 300);
     $('.side-menu-contain').toggle('slide', 300, 'swing');
     $( '.main-contain' ).animate({
       'margin-left' : $('.main-contain').css('margin-left') == '0px' ? '25%' : '0px'
     }, 300, 'swing');
   });
+  
   $('a.section-navi').on('click', function(e) {
     e.preventDefault();
     $(this).parent('section').toggleClass('collapsed', 600, 'easeOutElastic');
